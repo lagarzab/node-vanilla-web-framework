f0e1f4934bf7ff78541928424720a9ab04ae73ec
Bryan Garza <lagarzab@gmail.com>
2020-08-31 17:40:23 -0400

    Implemented 'vanilla' server

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const http = require('http')
+const vanilla = require('./src/vanilla-web-server')
+const server = vanilla()
 const PORT = 3333
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello World!')
-})
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}.`)
