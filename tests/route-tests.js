f8bf2e83c28ad55f08666110a842e6997a49227a
Bryan Garza <lagarzab@gmail.com>
2020-09-01 13:20:46 -0400

    Created vanilla assertion/test process for routes

---
+++ tests/route-tests.js
@@ -0,0 +1,32 @@
+const routeParser = require('../src/utils/url-regex')
+
+assert('This route with one param to pass').test("/products/114", "/products/:id").expect(true)
+assert('This route with two params to fail on route with one param').test("/products/114/testName", "/products/:id").expect(false)
+assert('This route with two params should pass to a route expecting two params').test("/products/114/testName", "/products/:id/:name").expect(true)
+assert('This route does not match, it should fail').test("/products/114/testName/any", "/products/:id/:name/another").expect(false)
+
+function assert(assertion) {
+    return {
+        assert: assertion,
+        route: null,
+        routeFormat: null,
+        result: null,
+        expect: function (expected) {
+            const status = this.result === expected
+            const msg = status ? 'Passed' : 'FAILED'
+            const color = status ? '\x1b[32m%s\x1b[0m' : '\x1b[47m\x1b[31m%s\x1b[0m'
+            console.log('')
+            console.log('\x1b[36m%s\x1b[0m', this.assert)
+            console.log(color, msg, ':', this.route, ' ==> ', this.routeFormat)
+            return this
+        },
+        test: function (route, routeFormat) {
+            this.route = route
+            this.routeFormat = routeFormat
+            const format = '^' + routeFormat + '$'
+            const regex = routeParser(format)
+            this.result = !!route.match(new RegExp(regex))
+            return this
+        }
+    }
+}
