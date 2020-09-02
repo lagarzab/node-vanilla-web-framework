b499137a0d42a0de2c3a2f49b592eb24b38638b8
Bryan Garza <lagarzab@gmail.com>
2020-09-02 10:40:12 -0400

    Added code to add routes to routing table

--- src/vanilla-web-server.js
+++ src/vanilla-web-server.js
@@ -1,13 +1,23 @@
 const http = require('http')
 
 function server () {
+    let routeTable = {}
     const svr = http.createServer((req, res) => {
         res.writeHead(200, {'Content-Type': 'text/plain'})
         res.end('Hello World!')
     })
 
     return {
-        listen: (port, cb) => svr.listen(port, cb)
+        listen: (port, cb) => svr.listen(port, cb),
+        get: (path, cb) => addRoute(path, 'get', cb),
+        post: (path, cb) => addRoute(path, 'post', cb),
+        put: (path, cb) => addRoute(path, 'put', cb),
+        delete: (path, cb) => addRoute(path, 'delete', cb)
+    }
+
+    function addRoute(path, method, cb) {
+        if (!routeTable[path]) routeTable[path] = {}
+        routeTable[path][method] = cb
     }
 }
 
